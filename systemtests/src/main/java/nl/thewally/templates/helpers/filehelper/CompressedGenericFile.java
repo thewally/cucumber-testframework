10ce01b6964e48fd5f8f5f3a2211a764d86a1547
Arjen van der Wal <arjen.van.der.wal@rabobank.nl>
2016-11-23 15:00:36 +0100

    Added decompres helper

--- systemtests/src/main/java/nl/thewally/templates/helpers/filehelper/CompressedGenericFile.java
+++ systemtests/src/main/java/nl/thewally/templates/helpers/filehelper/CompressedGenericFile.java
@@ -1,14 +1,80 @@
 package nl.thewally.templates.helpers.filehelper;
 
+import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 /**
  * Created by arjen on 23-11-16.
  */
 public class CompressedGenericFile extends GenericFile {
+    private static final Logger LOG = LoggerFactory.getLogger(CompressedGenericFile.class);
+    private static final int ONE_K = 1024;
+
     public CompressedGenericFile(String path, String filename) {
         super(path, filename);
+        String extension = FilenameUtils.getExtension(filename);
+        if(!extension.equals("zip")){
+            LOG.debug("This file with extension {} is not a compressed file", extension);
+        }
     }
 
     public void decompress() {
+        decompress(getPath());
+    }
 
+    public void decompress(String newPath) {
+        File newLocation = new File(newPath);
+        LOG.debug("Unzip {} to folder {}", getFullFilePath().getAbsolutePath(), newLocation.getAbsolutePath());
+        try {
+            //create output directory is not exists
+            if (!newLocation.exists() && !newLocation.mkdir()) {
+                LOG.error("Error making dir {}", newLocation.getAbsolutePath());
+            }
+            String lastFileName = null;
+            //get the zipped file list entry
+            //get the zip file content
+            try (ZipInputStream zis = new ZipInputStream(new FileInputStream(getFullFilePath()))) {
+                //get the zipped file list entry
+                ZipEntry ze = zis.getNextEntry();
+                lastFileName = null;
+                while (ze != null) {
+                    String fileName = ze.getName();
+                    final File newFile = new File(newLocation, fileName);
+                    LOG.debug("file unzip : {}", newFile.getAbsoluteFile());
+                    unzipSingleEntry(newFile, zis);
+                    lastFileName = newFile.getAbsoluteFile().toString();
+                    ze = zis.getNextEntry();
+                }
+                zis.closeEntry();
+            }
+            if (lastFileName == null) {
+                LOG.error("Error decompressing file {} no entries", getFullFilePath().getAbsolutePath());
+            }
+        } catch (IOException ex) {
+            LOG.error("Error decompressing file {}", getFullFilePath().getAbsolutePath(), ex);
+        }
+    }
+
+    private void unzipSingleEntry(final File newFile, final ZipInputStream zis) throws IOException {
+        //create all non exists folders
+        //else you will hit FileNotFoundException for compressed folder
+        if (!newFile.getParentFile().mkdirs()) {
+            LOG.warn("mkdirs for {} failed", newFile.getParent());
+        }
+        try (FileOutputStream fos = new FileOutputStream(newFile)) {
+            byte[] buffer = new byte[ONE_K];
+            int len;
+            while ((len = zis.read(buffer)) > 0) {
+                fos.write(buffer, 0, len);
+            }
+        }
     }
 }
--- systemtests/src/main/java/nl/thewally/templates/helpers/filehelper/GenericFile.java
+++ systemtests/src/main/java/nl/thewally/templates/helpers/filehelper/GenericFile.java
@@ -1,28 +1,53 @@
 package nl.thewally.templates.helpers.filehelper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.net.URI;
+
 /**
  * Created by arjen on 23-11-16.
  */
 public class GenericFile {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GenericFile.class);
+
     private String filename, path;
+    private File fullfilepath;
 
     public GenericFile(String path, String filename) {
         this.path = path;
         this.filename = filename;
+        this.fullfilepath = new File(path + filename);
     }
 
     public String getFilename() {
-        return "";
+        return filename;
     }
 
     public String getPath() {
-        return "";
+        return path;
+    }
+
+    public File getFullFilePath() {
+        return fullfilepath;
+    }
+
+    public URI getFileUri() {
+        return fullfilepath.toURI();
     }
 
     public void compress(){
     }
 
     public boolean isAvailable() {
-        return true;
+        if (fullfilepath.exists()) {
+            LOG.debug("File exists : {}", fullfilepath.getAbsolutePath());
+            return true;
+        } else {
+            LOG.debug("File not found! : {}", fullfilepath.getAbsolutePath());
+        }
+        return false;
     }
 }
