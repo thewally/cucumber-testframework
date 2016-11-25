05f74584f58ca0bca3d2b1f97b5449f8d6249c63
arjen <arjen@detesters.nl>
2016-11-25 20:25:02 +0100

    Added test.properties

--- systemtests/src/main/java/nl/thewally/templates/stepdefs/StepDefinitionsCompress.java
+++ systemtests/src/main/java/nl/thewally/templates/stepdefs/StepDefinitionsCompress.java
@@ -1,6 +1,7 @@
 package nl.thewally.templates.stepdefs;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -11,9 +12,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.FileAttribute;
+import java.util.Properties;
 
 /**
  * Created by arjen on 23-11-16.
@@ -25,9 +28,30 @@ public class StepDefinitionsCompress {
     private GenericFile newFile;
     private CompressedFile compressedFile;
 
+    private final Properties properties = new Properties();
+
+    private String workingDir;
+
+    private String getFilePath(String directory) {
+        String r;
+        if(directory.equals("")) {
+            r = System.getProperty(workingDir);
+        }
+        else {
+            r = System.getProperty(workingDir)+File.separator+directory;
+        }
+        return r;
+    }
+
+    @Before
+    public void setParameters()  throws IOException {
+        properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("test.properties"));
+        workingDir = properties.getProperty("working_dir");
+    }
+
     @Given("^create file with directory (.*) and filename (.*)$")
     public void createFileWithDirectoryAndFilename(String directory, String filename) throws Throwable {
-        relativePath = Files.createDirectory(new File(System.getProperty("user.home")+File.separator+directory).toPath());
+        relativePath = Files.createDirectory(new File(getFilePath(directory)).toPath());
         newFile = new GenericFile(relativePath, filename);
         newFile.createFile();
 
@@ -59,7 +83,7 @@ public class StepDefinitionsCompress {
 
     @Given("^select created compressed file with path (.*) and filename (.*)$")
     public void selectCreatedCompressedFileWithPathAndFilenameFile(String directory, String filename) throws Throwable {
-        relativePath = new File(System.getProperty("user.home")+File.separator+directory).toPath();
+        relativePath = new File(getFilePath(directory)).toPath();
         compressedFile = new CompressedFile(relativePath, filename);
 
         if(compressedFile.isAvailable()) {
@@ -79,7 +103,7 @@ public class StepDefinitionsCompress {
 
     @Then("^decompressed file (.*) is available in directory (.*)$")
     public void decompressedFileIsAvailableInDirectory(String filename, String directory) throws Throwable {
-        relativePath = new File(System.getProperty("user.home")+File.separator+directory).toPath();
+        relativePath = new File(getFilePath(directory)).toPath();
         newFile = new GenericFile(relativePath, filename);
         if(newFile.isAvailable()) {
             LOG.info("File {} is created", newFile.getFullFilePath());
